858b261194a2468fca8d37774b1b95ea6e2d0c7f
Javi <jbr.swork@gmail.com>
2022-12-17 16:23:10 +0100

    Day 4 - Puzzle 2

--- lib/day4.dart
+++ lib/day4.dart
@@ -1,9 +1,20 @@
+import 'dart:math';
+
 int puzzle1(List<String> assignments) {
   return assignments.where((element) {
     List<String> pairs = element.split(',');
     Assignment first = Assignment.from(pairs.first);
     Assignment second = Assignment.from(pairs.last);
     return first.isContainedIn(second) || second.isContainedIn(first);
+  }).length;
+}
+
+int puzzle2(List<String> assignments) {
+  return assignments.where((element) {
+    List<String> pairs = element.split(',');
+    Assignment first = Assignment.from(pairs.first);
+    Assignment second = Assignment.from(pairs.last);
+    return first.overlap(second);
   }).length;
 }
 
@@ -21,5 +32,10 @@ class Assignment {
   bool isContainedIn(Assignment assignment) {
     return assignment._lowerBound <= _lowerBound &&
         assignment._upperBound >= _upperBound;
+  }
+
+  bool overlap(Assignment assignment) {
+    return max(_lowerBound, assignment._lowerBound) <=
+        min(_upperBound, assignment._upperBound);
   }
 }
